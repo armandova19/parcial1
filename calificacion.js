8f3fee0c3f40dad3663fb11356325a044946bd0b
armandova19 <60717130+armandova19@users.noreply.github.com>
2020-02-10 16:37:11 -0600

    calificacion

---
+++ calificacion.js
@@ -0,0 +1,26 @@
+var  num=Math.floor(Math.random()*10);
+
+if(num==10)
+{
+    console.log("calificacion"+ " A");
+}
+else if(num==9)
+{
+    console.log("calificacion" + " B");
+}
+else if(num==8)
+{
+    console.log("calificacion" + " C");
+}
+else if(num==7)
+{
+    console.log("calificacion" + " D");
+}
+else if(num==6)
+{
+    console.log("calificacion" + " E");
+}
+else if(num>=1 && num<=5)
+{
+    console.log("calificacion"+ " F");
+}
