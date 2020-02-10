9405df5e42af1c74a6800ead37fb8b223ec85244
armandova19 <60717130+armandova19@users.noreply.github.com>
2020-02-10 16:39:16 -0600

    mayor
    el mayor de tres numeros

---
+++ mayor.js
@@ -0,0 +1,30 @@
+var  num1=Math.floor(Math.random()*10)+10;
+var  num2=Math.floor(Math.random()*10)+10;
+var  num3=Math.floor(Math.random()*10)+10;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+if(num1>num2)
+{
+    if(num1>num3)
+    {
+        console.log("mayor"+nunm1);
+    }
+    else
+    {
+        console.log("mayor"+num3);
+    }
+}
+else
+{
+    if(num2>num3)
+    {
+        console.log("mayor"+num2);
+    }
+    else
+    {
+        console.log("mayor"+num3);
+    }
+}
